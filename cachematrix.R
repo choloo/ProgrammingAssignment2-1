2404910b4b65d7ae5b16f67998a913aff535917e
choloo <87267401+choloo@users.noreply.github.com>
2021-07-11 21:10:48 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the creation of funtions aims to aims to exercise that either in the activity
+## to restore nor cache inverse of the matrix
 
-## Write a short comment describing this function
+## the makeCacheMatrix: has a function to store data's inverse by funtion that orders a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+set <- function(y) {
+x <<- y
+inv <<- NULL
+        
+        
+}
+        
+get <- function() x
+setInverse <- function(inverse) inv <<- inverse
+getInverse <- function() inv
+list(set = set,get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## the cacheSolve: was restored by the makeCacheMatrix. by the use cacheSolve whose function authenticates the matrix' inverse
+## the function of cacheSolve would attempt to recover inverse from cache due to the occurrence of the inverse's cmputation.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+inv <- x$getInverse()
+if (!is.null(inv)) {
+message("getting cached data")
+return(inv)}
+mat <- x$get()
+inv <- solve(mat, ...)
+x$setInverse(inv)
+inv}
